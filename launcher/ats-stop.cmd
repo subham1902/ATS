cc986a47b561746ed25ce2a2fb1aa2a067e201c0
Subham Panigrahi <subhampanigrahi1902@gmail.com>
2026-08-27 21:55:37 +0530

    ATS RELEASE: make user launchers self-resolving

--- launcher/ats-stop.cmd
+++ launcher/ats-stop.cmd
@@ -1,2 +1,4 @@
 @echo off
+for /f "tokens=2,*" %%A in ('reg query HKCU\Environment /v ATS_RELEASE_ROOT 2^>nul') do set "ATS_RELEASE_ROOT=%%B"
+if not defined ATS_RELEASE_ROOT exit /b 1
 pwsh.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%ATS_RELEASE_ROOT%\scripts\ats-stop.ps1" %*
