1f7c726ac5997c6d21cb72550126d4c5c7f0f422
Subham Panigrahi <subhampanigrahi1902@gmail.com>
2026-08-28 19:07:01 +0530

    feat(ui):operator-cockpit-v2-foundation

--- frontend/apps/control-center/app/page.tsx
+++ frontend/apps/control-center/app/page.tsx
@@ -1,5 +1,8 @@
 import { LiveOverview } from "../components/overview/LiveOverview";
+import { OperatorCockpitV2 } from "../components/cockpit/OperatorCockpitV2";
 
 export default function Page() {
-  return <LiveOverview />;
+  return process.env.NEXT_PUBLIC_ATS_OPERATOR_COCKPIT_V2 === "1"
+    ? <OperatorCockpitV2 />
+    : <LiveOverview />;
 }
